1e2b6986b9c2c2659506accaa70df72ac07ee71b
Note\jlabarca <jlabarca@dparadig.com>
2017-12-27 00:38:51 -0300

    UPD: Avances roles y privilegios

--- src/main/java/com/dparadig/auth_server/settings/configuration/AuthorizationServerConfiguration.java
+++ src/main/java/com/dparadig/auth_server/settings/configuration/AuthorizationServerConfiguration.java
@@ -4,6 +4,7 @@ import com.dparadig.auth_server.settings.security.oauth2.CustomUserDetailsServic
 import com.dparadig.auth_server.settings.security.jwt.JWTConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -15,6 +16,12 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${jwt.expiration-seconds}")
+    private int expirationSeconds;
+
+    @Value("${jwt.refresh-seconds}")
+    private int refreshSeconds;
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
@@ -39,8 +46,8 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
                 .withClient("dparadig").secret("ddp4r4d1g")
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
                 .scopes("read", "write")
-                .accessTokenValiditySeconds(6000)
-                .refreshTokenValiditySeconds(60000);
+                .accessTokenValiditySeconds(expirationSeconds)
+                .refreshTokenValiditySeconds(refreshSeconds);
     }
 
     @Override
