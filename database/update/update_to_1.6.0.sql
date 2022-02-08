2562539dffc6d1d9cfded1bd655b6de19115f34d
Julio Osorio <josorio@dparadig.com>
2022-02-08 11:26:01 -0500

    updateScriptFolder

--- database/update/update_to_1.6.0.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE dpa_user_dvel.customer_user ADD validate_access tinyint(1);
-
-UPDATE dpa_user_dvel.customer_user SET validate_access=0 WHERE customer_user_id = 184
--- database/update_to_1.6.0.sql
+++ database/update_to_1.6.0.sql
@@ -1,3 +1,4 @@
+/* Se creo la tabla user_session*/
 CREATE TABLE `user_sessions` (
   `session_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `customer_user_id` int(10) unsigned DEFAULT NULL,
@@ -6,4 +7,8 @@ CREATE TABLE `user_sessions` (
 ) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=l
 
 INSERT INTO dpa_user_dvel.user_sessions(customer_user_id, session_attempt_datetime)
-VALUES(20, CURRENT_TIMESTAMP);
+VALUES(200, CURRENT_TIMESTAMP);
+
+/* Se agrego la columna validate_access a la tabla customer_user*/
+ALTER TABLE dpa_user_dvel.customer_user ADD validate_access tinyint(1);
+UPDATE dpa_user_dvel.customer_user SET validate_access=0 WHERE customer_user_id = 184
