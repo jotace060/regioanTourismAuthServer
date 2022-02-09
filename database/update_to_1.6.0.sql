3bf5104b2ddffe31ecfd824e2f65377a9ef1b69f
Rodrigo Mujica <rmujica@dparadig.com>
2022-02-09 14:52:56 +0100

    FIX: Arreglar queries de update 1.6.0

--- database/update_to_1.6.0.sql
+++ database/update_to_1.6.0.sql
@@ -4,11 +4,11 @@ CREATE TABLE `user_sessions` (
   `customer_user_id` int(10) unsigned DEFAULT NULL,
   `session_attempt_datetime` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=l
+) 
 
 INSERT INTO dpa_user_dvel.user_sessions(customer_user_id, session_attempt_datetime)
 VALUES(200, CURRENT_TIMESTAMP);
 
 /* Se agrego la columna validate_access a la tabla customer_user*/
 ALTER TABLE dpa_user_dvel.customer_user ADD validate_access tinyint(1);
-UPDATE dpa_user_dvel.customer_user SET validate_access=0 WHERE customer_user_id = 184
+UPDATE dpa_user_dvel.customer_user SET validate_access=1
